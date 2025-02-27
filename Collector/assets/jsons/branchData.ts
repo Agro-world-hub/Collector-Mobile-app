9fe0f3f1ab6e04452623aef844a1c0ab86d7450a
KHRDilshan <127587609+KHRDilshan@users.noreply.github.com>
2025-02-27 09:19:13 +0530

    Delete Collector directory

--- Collector/assets/jsons/branchData.ts
+++
@@ -1,5 +0,0 @@
-export const branchData: Record<number, any> = {
-    7010: require('../jsons/Branches/7010.json'),
-    // Add all the mappings for other bank IDs
-  };
-  
--- Collector/i18n/translations/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as english } from './english.json'
-export { default as sinhala } from './sinhala.json'
-export { default as tamil } from './tamil.json'
